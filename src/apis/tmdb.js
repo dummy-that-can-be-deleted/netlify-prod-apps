ea083f558b3d9f3aed8c411ba6bf645b5fb81bf9
Panda Eat Bamboo <2495nmhst@relay.firefox.com>
2022-10-18 19:31:11 +0700

    feat: adding api key to code

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -5,7 +5,7 @@ const tmdbInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
     // TODO: Jangan lupa masukkan API_KEY yang benarnya di sini yah !
-    api_key: "INSERT_API_KEY_HERE",
+    api_key: "fcedb118a82853890434143eb4798d73",
   },
 });
 
